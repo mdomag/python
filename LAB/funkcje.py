1a5a3942fbb8949f5d09c53becc89ae7c4d1a92c
mdomag <willroowley@gmail.com>
2024-04-25 12:31:30 +0200

    lab7 plots

---
+++ LAB/funkcje.py
@@ -0,0 +1,13 @@
+#modul z funkcjami
+
+def f1(x: float) -> float:
+    return x**3
+
+def f2(x):
+    return x/7
+
+def main():
+    pass
+
+if __name__ == "main": #tylko tutaj wyprintuje bo to glowny watek(?s) 
+    print("??????????????")
